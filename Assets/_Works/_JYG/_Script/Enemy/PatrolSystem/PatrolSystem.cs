d742501b498d172ff8065707fabb7f0b2b4efe1c
Nugusaeyo <chsj0702@gmail.com>
2026-03-23 23:52:17 +0900

    Buildings

---
+++ Assets/_Works/_JYG/_Script/Enemy/PatrolSystem/PatrolSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace _JYG._Script.Enemy.PatrolSystem
+{
+    public class PatrolSystem : MonoBehaviour
+    {
+
+    }
+
+    [Serializable]
+    public class EnemyRoute
+    {
+        float waitTime = 2f;
+    }
+}
