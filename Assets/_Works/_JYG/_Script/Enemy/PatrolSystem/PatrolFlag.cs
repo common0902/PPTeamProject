8859fb21197790e67bdc1c74755c8f4b79879c20
Nugusaeyo <chsj0702@gmail.com>
2026-03-24 23:29:35 +0900

    Map

---
+++ Assets/_Works/_JYG/_Script/Enemy/PatrolSystem/PatrolFlag.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class PatrolFlag : MonoBehaviour
+{
+    public Transform GetFlagTransform() => transform;
+    public Vector3 GetFlagPosition() => transform.position;
+}
