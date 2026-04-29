77d7de7942b1a7cd29fc05ca457d0ba8bba7fad9
jjoung0911 <chjw091118@gmail.com>
2026-04-29 17:17:48 +0900

    feat : FovRendering

--- Assets/_Works/_CJW/Scripts/Objects/TestInteractObject.cs
+++ Assets/_Works/_CJW/Scripts/Objects/TestInteractObject.cs
@@ -1,12 +1,21 @@
+using _Script.ScriptableObject.Event;
+using _Works._CJW.Scripts.Events;
 using UnityEngine;
 
 namespace _Works._CJW.Scripts.Objects
 {
     public class TestInteractObject : AbstractInteractableObject
     {
+        [SerializeField] private EventChannelSO testEvent;
         protected override void HandleTriggerEnterEvent()
         {
             base.HandleTriggerEnterEvent();
+        }
+
+        protected override void HandleInteract(GameEvent evt)
+        {
+            base.HandleInteract(evt);
+            testEvent.RaiseEvent(InteractEvent.FireSabotage.Init(true));
         }
 
         protected override void HandleTriggerExitEvent()
