589885d2d2393ff48ef7398698ca65f940bb8e54
common0902 <minsoung200@gmail.com>
2026-04-29 09:00:59 +0900

    adf
    adsf

---
+++ Assets/_Works/_PMS/Code/FSM/TestStateMachineRunner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TestStateMachineRunner : MonoBehaviour
+{
+    private TestStateMachine stateMachine;
+
+    void Start()
+    {
+        stateMachine = new();
+        stateMachine.Setup(gameObject);
+    }
+
+    void Update()
+    {
+        stateMachine.Update();
+    }
+
+}
