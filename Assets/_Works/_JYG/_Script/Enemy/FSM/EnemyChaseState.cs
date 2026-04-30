47ce7efb86113884c43adfa467f2a35327a8971a
Nugusaeyo <chsj0702@gmail.com>
2026-04-30 12:36:39 +0900

    Enemy Caution UI
    에너미 경계수치 상승하는 시스템 만듦.

--- Assets/_Works/_JYG/_Script/Enemy/FSM/EnemyChaseState.cs
+++ Assets/_Works/_JYG/_Script/Enemy/FSM/EnemyChaseState.cs
@@ -25,13 +25,25 @@ namespace _Works._JYG._Script.Enemy.FSM
         }
         public override void Enter()
         {
-            base.Enter();
             _navmesh.Navmesh.isStopped = false;
             _navmesh.Navmesh.speed = _enemy.ChaseSpeed;
             
             _enemy.CallingPartner();    // n초 이내에 사살하지 못하면 모든 Enemy가 플레이어를 쫓는다.
             
             Debug.Log("CHASE!");
+            if(_targetCaster.TryGetTarget(out GameObject target)) //타겟을 한번 캐스팅 해보고 안닿으면 그때 애니메이션 재생.
+            //이렇게 하지 않으면 부자연스럽게 애니메이션이 끊기게 된다.
+            {
+                if(target.TryGetComponent<TestPPPP>(out TestPPPP test))
+                {
+                    Debug.Log("플레이어 찾음!!");
+                    _enemy.ChangeState((int)EnemyState.ATTACK);
+                }
+            }
+            else
+            {
+                base.Enter(); //애니메이션 재생
+            }
         }
 
         public override void Update()
