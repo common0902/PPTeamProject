5608662888fc195f7ef53e4a9cb70b20ea13ffd4
jjoung0911 <chjw091118@gmail.com>
2026-05-10 00:31:54 +0900

    feat: 수정

--- Assets/_Works/_CJW/Scripts/Events/CameraEvent.cs
+++ Assets/_Works/_CJW/Scripts/Events/CameraEvent.cs
@@ -1,10 +1,12 @@
 using _Script.ScriptableObject.Event;
+using _Works._CJW.Scripts.Rendering;
 
 namespace _Works._CJW.Scripts.Events
 {
     public static class CameraEvent
     {
-        public static TopViewEvent TopViewEvent = new();
+        public static readonly TopViewEvent TopViewEvent = new();
+        public static readonly RegisterFovEvent RegisterFovEvent = new();
     }
 
     public class TopViewEvent : GameEvent
@@ -14,6 +16,19 @@ namespace _Works._CJW.Scripts.Events
         public TopViewEvent Init(bool isTopView)
         {
             IsTopView = isTopView;
+            return this;
+        }
+    }
+    
+    public class RegisterFovEvent : GameEvent
+    {
+        public bool IsRegistered; // 등록인지 해제인지
+        public FOVRendering FovRendering; // 등록 또는 해제할 FOV 렌더링 컴포넌트
+
+        public RegisterFovEvent Init(bool isRegistered, FOVRendering fovRendering)
+        {
+            IsRegistered = isRegistered;
+            FovRendering = fovRendering;
             return this;
         }
     }
