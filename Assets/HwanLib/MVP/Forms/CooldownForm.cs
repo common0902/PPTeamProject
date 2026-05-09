04fdff91561b96e63d36b326de81bda8bd0358cc
Hwan <hwanji203@gmail.com>
2026-05-09 17:12:20 +0900

    feat: CooldowmForm

--- Assets/HwanLib/MVP/Forms/CooldownForm.cs
+++ Assets/HwanLib/MVP/Forms/CooldownForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using HwanLib.MVP.Forms.Module.Cooldown;
+using HwanLib.MVP.Forms.Module.Gauge;
 using HwanLib.MVP.System;
 using HwanLib.MVP.System.BaseMVP.Form;
 using HwanLib.MVP.UIData;
@@ -8,85 +9,35 @@ namespace HwanLib.MVP.Forms
 {
     public class CooldownForm : AbstractVisualForm
     {
-        private enum CooldownType
-        {
-            PosY,
-        }
-        
-        [SerializeField] private CooldownType cooldownType = CooldownType.PosY;
-
+        private GaugeType _gaugeType;
         private AbstractCooldown _cooldown;
-        
-        protected void InitCooldownForm(float cooldownTime)
+        private Coroutine _cooldownCoroutine;
+
+        public void InitCooldownForm(GaugeType gaugeType)
         {
-            switch (cooldownType)
+            _gaugeType = gaugeType;
+            
+            switch (_gaugeType)
             {
-                case CooldownType.PosY:
+                case GaugeType.PosY:
                     _cooldown = new PosYCooldown();
                     break;
             }
             
-            _cooldown.InitCooldown(gameObject, cooldownTime);
+            _cooldown.InitGauge(gameObject);
         }
 
         protected override void UpdateVisual(UIParam data)
         {
-            float ratio = ((UIFloatParam)data).Value;
+            UICooldownParam cooldownData = (UICooldownParam)data;
             
-            _cooldown.CooldownRatio = ratio;
-            if (ratio != 0)
-                StartCoroutine(_cooldown.StartCooldown());
-        }
-
-        private abstract class AbstractCooldown
-        {
-            public float CooldownRatio
-            {
-                get => Mathf.Clamp01(_currentCooldown / _cooldownTime);
-                set
-                {
-                    _currentCooldown = _cooldownTime * Mathf.Clamp01(value);
-                    SetCooldown();
-                }
-            }
-
-
-            private float _currentCooldown;
-            private float _cooldownTime;
+            if (_cooldown.GaugeRatio != 0)
+                StopCoroutine(_cooldownCoroutine);
             
-            public virtual void InitCooldown(GameObject gameObject, float cooldownTime)
-            {
-                _cooldownTime = cooldownTime;
-                CooldownRatio = 0;
-            }
-
-            public IEnumerator StartCooldown()
-            {
-                CooldownRatio = 1;
-                while (CooldownRatio != 0)
-                {
-                    yield return null;
-                    _currentCooldown -= Time.deltaTime;
-                    SetCooldown();
-                }
-                CooldownRatio = 0;
-            }
-
-            protected abstract void SetCooldown();
-        }
-
-        private class PosYCooldown : AbstractCooldown
-        {
-            private Transform _targetTransform;
-
-            public override void InitCooldown(GameObject gameObject, float cooldownTime)
-            {
-                base.InitCooldown(gameObject, cooldownTime);
-                _targetTransform = gameObject.transform;
-            }
-
-            protected override void SetCooldown()
-                => _targetTransform.localScale = new Vector3(1, CooldownRatio, 1);
+            _cooldown.SetCoolTime(cooldownData.Cooldown);
+            _cooldown.GaugeRatio = cooldownData.Ratio;
+            if (cooldownData.Ratio != 0)
+                _cooldownCoroutine = StartCoroutine(_cooldown.StartCooldown());
         }
     }
 }
---
+++ Assets/HwanLib/MVP/Forms/Module/Cooldown/AbstractCooldown.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using HwanLib.MVP.Forms.Module.Gauge;
+using UnityEngine;
+
+namespace HwanLib.MVP.Forms.Module.Cooldown
+{
+    internal abstract class AbstractCooldown : AbstractGauge
+    {
+        private float _currentTime;
+        private float _coolTime;
+
+        public override float GaugeRatio
+        {
+            get
+            {
+                if (_coolTime <= 0)
+                    return 0;
+                return Mathf.Clamp01(_currentTime / _coolTime);
+            }
+            set
+            {
+                _currentTime = _coolTime * Mathf.Clamp01(value);
+                SetGauge();
+            }
+        }
+
+        public void SetCoolTime(float coolTime)
+            => _coolTime = coolTime;
+
+        public IEnumerator StartCooldown()
+        {
+            while (GaugeRatio != 0)
+            {
+                yield return null;
+                _currentTime -= Time.deltaTime;
+                SetGauge();
+            }
+            GaugeRatio = 0;
+        }
+    }
+}
