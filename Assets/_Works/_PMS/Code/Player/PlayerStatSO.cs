7b71ba950b526f4876cd8643ca8ee312633fb220
common0902 <minsoung200@gmail.com>
2026-05-11 23:44:56 +0900

    ddd

--- Assets/_Works/_PMS/Code/Player/PlayerStatSO.cs
+++ Assets/_Works/_PMS/Code/Player/PlayerStatSO.cs
@@ -4,7 +4,7 @@
 public class PlayerStatSO : ScriptableObject
 {
     [field: SerializeField] public bool IsGun { get; private set; } = false;
-    [field: SerializeField] public float Hp { get; private set; }
+    [field: SerializeField] public float Hp { get; private set; } = 100f;
     [field: SerializeField] public float ViewMapCooldown { get; private set; } = 5.0f;
     [field: SerializeField] public float RunCooldown { get; private set; } = 2.0f;
 
