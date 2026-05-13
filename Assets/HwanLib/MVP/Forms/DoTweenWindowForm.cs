810feb815a1a182339e8c9568dbc8c3e2583a62e
Hwan <hwanji203@gmail.com>
2026-05-13 19:33:24 +0900

    feat: AbstractPopupView

--- Assets/HwanLib/MVP/Forms/DoTweenWindowForm.cs
+++ Assets/HwanLib/MVP/Forms/DoTweenWindowForm.cs
@@ -8,8 +8,8 @@ namespace HwanLib.MVP.Forms
 {
     public class DoTweenWindowForm : BaseForm
     {
-        [SerializeField] private float openDuration = 0.25f;
-        [SerializeField] private float closeDuration = 0.225f;
+        [field: SerializeField] public float OpenDuration { get; private set; } = 0.25f;
+        [field: SerializeField] public float CloseDuration { get; private set; } = 0.225f;
         
         public event Action OnAnimationEnd;
 
@@ -31,7 +31,7 @@ namespace HwanLib.MVP.Forms
             transform.localScale = Vector3.zero;
 
             _sequence = DOTween.Sequence();
-            float curDuration = Mathf.Clamp01(1 - transform.localScale.x) * openDuration;
+            float curDuration = Mathf.Clamp01(1 - transform.localScale.x) * OpenDuration;
             _sequence.Append(transform.DOScale(Vector3.one, curDuration).SetEase(Ease.InCirc))
                 .SetUpdate(true)
                 .OnComplete(() =>
@@ -52,7 +52,7 @@ namespace HwanLib.MVP.Forms
             transform.localScale = Vector3.one;
 
             _sequence = DOTween.Sequence();
-            float curDuration = transform.localScale.x * closeDuration;
+            float curDuration = transform.localScale.x * CloseDuration;
             _sequence.Append(transform.DOScale(Vector3.zero, curDuration).SetEase(Ease.InBack))
                 .SetUpdate(true)
                 .OnComplete(() =>
--- Assets/HwanLib/MVP/System/BaseMVP/BaseView.cs
+++ Assets/HwanLib/MVP/System/BaseMVP/BaseView.cs
@@ -22,7 +22,7 @@ namespace HwanLib.MVP.System.BaseMVP
             _formDict = new Dictionary<int, BaseForm>();
             _lookup = new Dictionary<(Action, int), Action<int>>();
             
-            Transform[] children = root.GetComponentsInChildren<Transform>();
+            Transform[] children = root.GetComponentsInChildren<Transform>(true);
             foreach (var formData in formDataList)
             {
                 Transform child = children[formData.childIndex];
@@ -52,6 +52,11 @@ namespace HwanLib.MVP.System.BaseMVP
         {
             UpdateView();
             RootCanvas.gameObject.SetActive(true);
+        }
+
+        public virtual void CloseView()
+        {
+            RootCanvas.gameObject.SetActive(false);
         }
 
         public virtual void OnDestroyView()
