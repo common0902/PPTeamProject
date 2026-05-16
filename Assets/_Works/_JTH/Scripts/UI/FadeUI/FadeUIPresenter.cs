a00c18c6e2d43d965a52f4cc6b7ae222b9de45ac
Hwan <hwanji203@gmail.com>
2026-05-17 04:00:23 +0900

    feat: FadeStage

---
+++ Assets/_Works/_JTH/Scripts/UI/FadeUI/FadeUIPresenter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Script.ScriptableObject.Event;
+using _Works._JTH.Scripts.UI.Event;
+using HwanLib.MVP.System.BaseMVP;
+using HwanLib.MVP.System.GenerateUI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Works._JTH.Scripts.UI.FadeUI
+{
+    public class FadeUIPresenter : BasePresenter
+    {
+        [SerializeField] private EventChannelSO openUIChannel;
+        [SerializeField] private int stageStartIndex;
+        [SerializeField] private float fadeDuration = 2;
+        [SerializeField] private float drawDuration = 2;
+        [SerializeField] private float waitForCloseDuration = 0.25f;
+        
+        private FadeUIModel _fadeModel;
+        private FadeUIView _fadeView;
+        
+        private int _nextStageIndex;
+        
+        public override void InitializePresenter(List<FormData> formData, Type viewType, Type modelType)
+        {
+            base.InitializePresenter(formData, viewType, modelType);
+
+            _fadeModel = (FadeUIModel)Model;
+            _fadeView = (FadeUIView)View;
+            
+            _fadeView.FadeDuration = fadeDuration;
+            _fadeView.DrawDuration = drawDuration;
+            
+            openUIChannel.AddListener<OpenFadeUIEvent>(OpenUI);
+            _fadeView.BackgroundForm.OnFadeEnd += CompleteFadeHandler;
+        }
+
+        private void SceneLoadedHandler(Scene _, LoadSceneMode __)
+            => StartCoroutine(WaitForCloseView());
+
+        private IEnumerator WaitForCloseView()
+        {
+            yield return new WaitForSecondsRealtime(waitForCloseDuration);
+            _fadeView.StartClose();
+        }
+
+        private void OpenUI(OpenFadeUIEvent data)
+        {
+            _nextStageIndex = data.SceneIndex + 1;
+            _fadeView.NextStage = (data.SceneIndex - stageStartIndex + 2).ToString();
+            _fadeModel.CurrentStage = data.SceneIndex - stageStartIndex + 1;
+
+            _fadeView.DrawNextDayText = data.DrawNextDayText;
+            _fadeView.DrawCurDayText = data.DrawCurDayText;
+            _fadeView.OpenView();
+        }
+
+        private void CompleteFadeHandler(bool fadeIn)
+        {
+            if (fadeIn)
+            {
+                SceneManager.LoadScene(_nextStageIndex);
+                SceneManager.sceneLoaded += SceneLoadedHandler;
+            }
+            else
+            {
+                _fadeView.CloseView();
+                SceneManager.sceneLoaded -= SceneLoadedHandler;
+            }
+        }
+        
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            
+            _fadeView.BackgroundForm.OnFadeEnd -= CompleteFadeHandler;
+        }
+    }
+}
