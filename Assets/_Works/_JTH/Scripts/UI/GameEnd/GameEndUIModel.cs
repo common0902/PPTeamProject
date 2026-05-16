d46ae53c1a2313797530b519b693a5d9838c2f89
Hwan <hwanji203@gmail.com>
2026-05-17 04:01:10 +0900

    feat: GameEndUI

---
+++ Assets/_Works/_JTH/Scripts/UI/GameEnd/GameEndUIModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using _Script.ScriptableObject.Event;
+using _Works._JTH.Scripts.UI.Event;
+using HwanLib.MVP.System.AbstractMVP.SaveMVP;
+using HwanLib.MVP.UIData;
+
+namespace _Works._JTH.Scripts.UI.GameEnd
+{
+    public class GameEndUIModel : ISaveableModel
+    {
+        private string _savedStage;
+        private const string GameOverTitle = "GAME OVER";
+        private const string GameClearTitle = "CLEAR";
+         
+        public bool IsGameOver { get; set; }
+        public int StageStartIndex { get; set; }
+        public Action CloseViewAction { get; set; }
+        public EventChannelSO OpenUIChannel { get; set; }
+        
+        private int NextStage => int.Parse(_savedStage) + 1;
+        
+        public void SetDefaultValue()
+        {
+            _savedStage = "0";
+        }
+
+        public string StoreData()
+        {
+            return NextStage.ToString();
+        }
+
+        public void RestoreData(string data)
+            => _savedStage = data;
+        
+        private UIParam UpdateTitleText() => UIParams.UIStringParam.Init(IsGameOver ? GameOverTitle : GameClearTitle);
+
+        private UIParam UpdateDayText() => UIParams.UIStringParam.Init(_savedStage);
+        
+        private UIParam UpdateDescText() => UIParams.UIStringParam.Init(_savedStage);
+
+        private void ContinueBtnHandler(UIParam clickData)
+            => OpenUIChannel.RaiseEvent(OpenUIEvents.OpenFadeUIEvent
+                .Init(StageStartIndex + NextStage - 2, false, true));
+        
+        private void NextBtnHandler(UIParam clickData)
+        {
+            CloseViewAction?.Invoke();
+            OpenUIChannel.RaiseEvent(OpenUIEvents.OpenFadeUIEvent
+                .Init(StageStartIndex + NextStage - 1, true, true));
+        }
+
+        private void QuitBtnHandler(UIParam clickData)
+        {
+            CloseViewAction?.Invoke();
+            OpenUIChannel.RaiseEvent(OpenUIEvents.OpenFadeUIEvent.Init(0, true, false));
+        }
+    }
+}
---
+++ Assets/_Works/_JTH/Scripts/UI/GameEnd/GameEndUIView.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using HwanLib.MVP.Forms;
+using HwanLib.MVP.System;
+using HwanLib.MVP.System.BaseMVP;
+using HwanLib.MVP.System.GenerateUI;
+using UnityEngine;
+
+namespace _Works._JTH.Scripts.UI.GameEnd
+{
+    public class GameEndUIView : BaseView
+    {
+        private enum UIState
+        {
+            GameOver,
+            GameClear,
+        }
+
+        private AccessForm _clearUI;
+        private AccessForm _gameOverUI;
+        private BackgroundForm _background;
+        
+        public float FadeDuration { get; set; }
+        public Color FadeColor { get; set; }
+        
+        public void SetGameState(bool isGameOver) => UpdateState(isGameOver ? UIState.GameOver : UIState.GameClear);
+
+        public override void InitializeView(GameObject root, List<FormData> formDataList, FormInteracted formInteractedHandler,
+            UpdateForm updateFormHandler)
+        {
+            base.InitializeView(root, formDataList, formInteractedHandler, updateFormHandler);
+            
+            _clearUI = GetForm<AccessForm>((int)GameEndUIEnum.ClearUI);
+            _gameOverUI = GetForm<AccessForm>((int)GameEndUIEnum.GameOverUI);
+            _background = GetForm<BackgroundForm>((int)GameEndUIEnum.Background);
+        }
+
+        public override void OpenView()
+        {
+            base.OpenView();
+            
+            _background.DoFade(FadeDuration, FadeColor);
+        }
+
+        public override void CloseView()
+        {
+            base.CloseView();
+            
+            _background.DoFade(false, 0);
+        }
+
+        private void UpdateState(UIState state)
+        {
+            _clearUI.gameObject.SetActive(false);
+            _gameOverUI.gameObject.SetActive(false);
+            
+            switch (state)
+            {
+                case UIState.GameOver:
+                    _gameOverUI.gameObject.SetActive(true);
+                    break;
+                case UIState.GameClear:
+                    _clearUI.gameObject.SetActive(true);
+                    break;
+            }
+        }
+    }
+}
