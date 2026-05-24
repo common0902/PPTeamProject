95c87fc96a23686573c9c5d8f2a097d6f2892f6b
Hwan <hwanji203@gmail.com>
2026-05-25 00:17:14 +0900

    .

--- Assets/_Works/_JTH/Scripts/UI/Tooltip/TooltipUIPresenter.cs
+++ Assets/_Works/_JTH/Scripts/UI/Tooltip/TooltipUIPresenter.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using _Script.ScriptableObject.Event;
 using _Works._CJW.Scripts.Events;
 using _Works._CJW.Scripts.Objects.Sabotage;
-using HwanLib.MVP.Forms;
 using HwanLib.MVP.System.BaseMVP;
 using HwanLib.MVP.System.GenerateUI;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 namespace _Works._JTH.Scripts.UI.Tooltip
 {
