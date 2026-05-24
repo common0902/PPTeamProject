a52630fd5e344d6b21710d7a84df3775ba29b962
jjoung0911 <chjw091118@gmail.com>
2026-05-24 22:02:27 +0900

    feat: None

--- Assets/_Works/_CJW/Scripts/CameraController.cs
+++ Assets/_Works/_CJW/Scripts/CameraController.cs
@@ -143,20 +143,14 @@ namespace _Works._CJW.Scripts
                 }
                 else
                 {
-                    float movePercent =
-                        Mathf.InverseLerp(
-                            rotateStartPercent,
-                            1f,
-                            percent);
+                    float movePercent = Mathf.InverseLerp
+                    (rotateStartPercent, 1f, percent);
 
                     float heightCurve =
                         transitionCurve.Evaluate(movePercent);
 
-                    _thirdPersonFollow.VerticalArmLength =
-                        Mathf.Lerp(
-                            startVal,
-                            resultHeight,
-                            heightCurve);
+                    _thirdPersonFollow.VerticalArmLength = Mathf.Lerp
+                    (startVal, resultHeight, heightCurve);
                 }
 
                 yield return null;
